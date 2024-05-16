3b31b50dbd8ef5483eca99ccbaf6ce8bc3df236c
Emanuele Giacinti <emanuele.giacinti@advinser.it>
2024-05-16 22:16:57 +0200

    feat: getPaginatedBreweryList function and inject into view

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\BreweryService;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\View\View;
+
+class HomeController extends Controller
+{
+    public function render(): View
+    {
+        $brewery = $this->getPaginatedBreweryList();
+
+        return view('dashboard', [
+            'brewery' => $brewery
+        ]);
+    }
+
+    public function getPaginatedBreweryList(): array | string
+    {
+        try {
+            $response = Http::get('https://api.openbrewerydb.org/v1/breweries');
+
+            return $response->json();
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            Log::error($e->getTraceAsString());
+        }
+
+        return "No breweries found";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::middleware(['auth', 'verified'])->group(function (){
+    Route::get('/', [HomeController::class, 'render'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/dashboard', [HomeController::class, 'render'])->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
