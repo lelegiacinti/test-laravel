ac28ad469e1e1525a08d4e8feb12249cea781a32
Emanuele Giacinti <emanuele.giacinti@advinser.it>
2024-05-18 01:12:35 +0200

    fix: added BreweryService

--- app/Services/BreweryService.php
+++ app/Services/BreweryService.php
@@ -2,9 +2,41 @@
 
 namespace App\Services;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class BreweryService
 {
+    public function getPaginatedBreweryList(Request $request): ?array
+    {
+        try {
+            if (null !== $request->getQueryString()){
+                $response = Http::get(config('app.breweries_url') . '?' . $request->getQueryString());
+            } else {
+                $response = Http::get(config('app.breweries_url'));
+            }
 
+            return $response->json();
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            Log::error($e->getTraceAsString());
+        }
+
+        return null;
+    }
+
+    public function getMetadataBreweryList(): ?array
+    {
+        try {
+            $response = Http::get(config('app.breweries_url') . '/meta');
+
+            return $response->json();
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            Log::error($e->getTraceAsString());
+        }
+
+        return null;
+    }
 }
