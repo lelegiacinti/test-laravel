089ef074cc408b1ae0422ef6781642cbde5de234
Emanuele Giacinti <emanuele.giacinti@advinser.it>
2024-05-18 01:13:12 +0200

    fix: redirect to dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function (){
-    Route::get('/', [HomeController::class, 'render'])->name('home');
-
-    Route::get('/dashboard', [HomeController::class, 'render'])->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('home');
+    Route::get('/dashboard', [DashboardController::class, 'render'])->name('dashboard');
 });
 
 Route::middleware('auth')->group(function () {
